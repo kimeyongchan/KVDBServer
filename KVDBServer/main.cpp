aae335e72ed638eb3364869bd7051b1f23eb7f8f
seoyujin <mildtuna22@gmail.com>
2015-08-23 00:12:43 +0900

    .

--- KVDBServer/main.cpp
+++ KVDBServer/main.cpp
@@ -2,10 +2,6 @@
 #include "Log.h"
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include "IOManager.h"
-#include "RequestInfo.h"
-
 
 
 int main(int argc, const char * argv[])
@@ -18,44 +14,7 @@ int main(int argc, const char * argv[])
         return -1;     
     }
     
-
-    // KVDBServer::getInstance()->Run();
-    
-    InsertDirectoryRequestInfo reqDirInfo_a;
-    reqDirInfo_a.key ="a";
-    
-    
-    InsertDirectoryRequestInfo reqDirInfo_ab;
-    reqDirInfo_ab.key ="a/b";
-    
-    
-    InsertDirectoryRequestInfo reqDirInfo_abc;
-    reqDirInfo_abc.key ="a/b/c";
-    
-    
-    InsertRequestInfo reqInfo;
-    reqInfo.key ="a/b/c/d";
-    reqInfo.value = "he";
-
-    
-    IOManager ioMgr;
-    ioMgr.processInsert(&reqDirInfo_a);
-    ioMgr.processInsert(&reqDirInfo_ab);
-    ioMgr.processInsert(&reqDirInfo_abc);
-    ioMgr.processInsert(&reqInfo);
-
-    FindRequestInfo findReqInfo;
-    findReqInfo.key = "a/b/c/d";
-    
-    //ioMgr.processFind(&findReqInfo);
-    
-     DeleteRequestInfo delReqInfo;
-     delReqInfo.key = "a/b/c/d";
-    
-    ioMgr.processDelete(&delReqInfo);
-    
-  //  ioMgr.processFind(&findReqInfo);
-    
+     KVDBServer::getInstance()->Run();
     
     return 0;
     
